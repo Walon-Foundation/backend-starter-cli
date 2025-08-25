2bfae52c7c8913cf10f2bac3fcd1c489e81096a1
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-08-25 22:40:35 +0000

    fix the template folder issue in this file

--- internals/frameworks/framework.go
+++ internals/frameworks/framework.go
@@ -8,13 +8,13 @@ type Framework struct {
 }
 
 var All = []Framework{
-	{Name: "express-js", UseOfficial: false, TemplateDir: "templates/express-js"},
-	{Name: "express-ts", UseOfficial: false, TemplateDir: "templates/express-ts"},
-	{Name: "fastapi", UseOfficial: false, TemplateDir: "templates/fastapi"},
-	{Name: "gin", UseOfficial: false, TemplateDir: "templates/gin"},
-	{Name: "fiber", UseOfficial: false, TemplateDir: "templates/fiber"},
+	{Name: "express-js", UseOfficial: false, TemplateDir: "express-js"},
+	{Name: "express-ts", UseOfficial: false, TemplateDir: "express-ts"},
+	{Name: "fastapi", UseOfficial: false, TemplateDir: "fastapi"},
+	{Name: "gin", UseOfficial: false, TemplateDir: "gin"},
+	{Name: "fiber", UseOfficial: false, TemplateDir: "fiber"},
 	// If we add frameworks with official CLIs:
 	{Name: "hono", UseOfficial: true, CliCommand: []string{"npx", "create-hono@latest"}},
 	{Name: "nextjs", UseOfficial: true, CliCommand: []string{"npx", "create-next-app@latest"}},
-	{Name: "nestjs", UseOfficial: true, CliCommand: []string{"npx", "create-next-sever"}},
+	{Name: "nestjs", UseOfficial: true, CliCommand: []string{"npx", "@nestjs/cli@latest"}},
 }
