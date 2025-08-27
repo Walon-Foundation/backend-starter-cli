904c266d54c96cb5accfdaf5cb99003cdd590f65
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-08-27 12:33:44 +0000

    fix the template for express-ts

--- internals/scaffold/templates/express-ts/src/index.ts
+++ internals/scaffold/templates/express-ts/src/index.ts
@@ -1,13 +1,26 @@
 import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { corsOptions } from "./configs/corsOption";
 
+config()
+
+//making the server
 const app = express()
 
+//middleware
 app.use(express.json())
+app.use(cors(corsOptions))
 
+
+//test route
 app.get("/", async(req, res) => {
     return res.send("hello world")
 })
 
-app.listen(3000, () => {
-    console.log(`server is running on http://localhost:${3000}`)
+
+//server starting point
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`)
 })
