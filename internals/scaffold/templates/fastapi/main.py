f79ea14770120372ccce6548d5ec0adcd59025bb
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-08-27 12:46:31 +0000

    fix the fastapi template

--- internals/scaffold/templates/fastapi/main.py
+++ internals/scaffold/templates/fastapi/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
+#loading in the .env
+load_dotenv()
 
 app = FastAPI()
 
+#origins to be allowed
+origins = [
+    "add the url you want to allow"
+]
+
+#adding the cors middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
